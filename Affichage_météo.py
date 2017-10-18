ff9d3ba849489aa22ebd3740434fad8353247c2c
Valentin1laurent <32638769+Valentin1laurent@users.noreply.github.com>
2017-10-18 15:30:41 +0200

    test

--- Affichage_météo.py
+++ Affichage_météo.py
@@ -8,4 +8,4 @@
     temp0=int(data['list'][0]['main']['temp']-273)
     desc0=data['list'][0]['weather'][0]['description']
     temp1=int(data['list'][1]['main']['temp']-273)
-    desc1=data['list'][1]['weather'][0]['description']
+    desc1=data['list'][1]['weather'][0]['description'
